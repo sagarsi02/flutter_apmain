956de515887f0abe3b3b12e15e53bb6285fb1e6d
gp-sagar <sagarsingh@grampower.com>
2022-11-15 17:28:40 +0530

    color remove

--- lib/answer.dart
+++ lib/answer.dart
@@ -6,12 +6,11 @@ class Answer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var colors;
     return Container(
       width: double.infinity,
       child: ElevatedButton(
         child: Text('Answer 1'),
-        onPressed: selectHandler(),
+        onPressed: null,
       ),
     );
   }
